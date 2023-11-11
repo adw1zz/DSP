18a1a1c8367a19af5e21ef436730a0f8ebe6cec0
adw1zz <vanae089@gmail.com>
2023-11-11 20:15:57 +0300

    v1 of box blur gauss

--- lab2/index.js
+++ lab2/index.js
@@ -1,6 +1,11 @@
+const readImage = require('./services/read-image-file');
+const GaussByBoxBlur = require('./services/gauss/guass-by-box-blur');
+const writeImage = require('./services/write-image-file');
 
-const main = () => {
-    
-}
+const main = async () => {
+    const image = await readImage('./source/exmpl.png');
+    GaussByBoxBlur.gaussBoxBlur(image);
+    writeImage(GaussByBoxBlur.res, './source/blur.png')
+}   
 
 main()
--- lab2/services/gauss/guass-by-box-blur.js
+++ lab2/services/gauss/guass-by-box-blur.js
@@ -1,5 +1,39 @@
 class GaussByBoxBlur {
 
+    #count = 0;
+    res;
+
+    gaussBoxBlur(image) {
+        const result = [];
+        this.#count++;
+        for (let i = 0; i < image.length; i++) {
+            result.push([]);
+            for (let j = 0; j < image[i].length; j++) {
+                let sumR = 0, sumG = 0, sumB = 0;
+                let count = 0;
+                for (let dx = -1; dx <= 1; dx++) {
+                    for (let dy = -1; dy <= 1; dy++) {
+                        const nx = i + dx;
+                        const ny = j + dy;
+                        if (nx >= 0 && nx < image.length && ny >= 0 && ny < image[i].length) {
+                            sumR += image[nx][ny][0];
+                            sumG += image[nx][ny][1];
+                            sumB += image[nx][ny][2];
+                            count++;
+                        }
+                    }
+                }
+                result[i][j] = [Math.floor(sumR / count), Math.floor(sumG / count), Math.floor(sumB / count)];
+            }
+        }
+        if (this.#count > 20) {
+            return;
+        } else {
+            this.res = result;
+            this.gaussBoxBlur(result);
+        }
+    }
+
 }
 
-module.exports = GaussByBoxBlur;
+module.exports = new GaussByBoxBlur();
