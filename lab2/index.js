e99ed4b01ee27e564fcee54c7e2e07ff0320e112
adw1zz <vanae089@gmail.com>
2023-11-14 12:43:12 +0300

    add input kernel size

--- lab2/index.js
+++ lab2/index.js
@@ -7,10 +7,15 @@ const main = async () => {
     while (true) {
         console.clear();
         const value = await readLineAsync("-------MENU-------\n1 -- Gauss by blur-box\n2 -- Median Filter\n3 -- Sobel Operator\n");
+        let kernelSize = null;
+        if (Number(value) === 1 || Number(value) === 2) {
+            const inp = await readLineAsync("Kernel size: ");
+            kernelSize = Number(inp);
+        }
         const obj = menu(Number(value));
         const image = await readImage('./source/exmpl2.png');
         const start = performance.now();
-        const result = obj.process(image);
+        const result = obj.process(image, kernelSize);
         const end = performance.now();
         console.log(`Process time: ${end - start}ms || ${(end - start) / 1000}sec`)
         writeImage(result, `./source/${obj.constructor.name}.png`)
